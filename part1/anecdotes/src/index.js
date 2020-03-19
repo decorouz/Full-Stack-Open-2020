836fd1cec4ea9102e7283edae3abb2239e8c86e1
Biola <abiola2326@gmail.com>
2020-03-19 17:22:56 +0100

    1.12*: anecdotes

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -5,27 +5,46 @@ const Button = props => {
   return <button onClick={props.handleClick}>{props.text}</button>;
 };
 
+const MaxVote = props => {
+  const maxVote = Math.max(...props.vote);
+  const indexofmaxVote = props.vote.indexOf(maxVote);
+
+  if (maxVote === 0) {
+    return 'The votes are not in yet. Be the first to vote';
+  }
+
+  return (
+    <div>
+      {props.anecdote[indexofmaxVote]}
+      <br /> has {maxVote} votes
+    </div>
+  );
+};
+
 const App = props => {
   const [selected, setSelected] = useState(0);
   const [votes, setVotes] = useState(new Array(props.anecdotes.length).fill(0));
 
   const randNumber = Math.floor(Math.random() * anecdotes.length);
-  const handleRandomNumber = () => {
-    return setSelected(randNumber);
-  };
+
+  const handleRandomAnecdote = () => setSelected(randNumber);
 
   const handleVoting = () => {
     const copy = [...votes];
     copy[selected] += 1;
+
     return setVotes(copy);
   };
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <div>{props.anecdotes[selected]}</div>
       <div>has {votes[selected]} votes</div>
       <Button handleClick={handleVoting} text="vote" />
-      <Button handleClick={handleRandomNumber} text="next anecdote" />
+      <Button handleClick={handleRandomAnecdote} text="next anecdote" />
+      <h3>anecdote with the most votes</h3>
+      <MaxVote anecdote={props.anecdotes} vote={votes} />
     </div>
   );
 };
