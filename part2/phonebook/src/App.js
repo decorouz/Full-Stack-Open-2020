d582b8ee5480727eea8d3a3e2ac475189c89920c
Biola <abiola2326@gmail.com>
2020-03-22 15:31:17 +0100

    2.8 phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,32 @@
 import React, { useState } from 'react';
 
 const Person = props => {
-  return <div>{props.person.name}</div>;
+  return (
+    <>
+      <table>
+        <tbody>
+          <tr>
+            <td>{props.person.name}</td>
+            <td>{props.person.number}</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '4444-44444-444' }
+  ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const addPerson = event => {
     event.preventDefault();
     const newPerson = {
       name: newName,
+      number: newNumber,
       date: new Date().toISOString()
     };
     const isFound = persons.find(
@@ -29,12 +44,19 @@ const App = () => {
     setNewName(event.target.value);
   };
 
+  const handleNumberChange = event => {
+    setNewNumber(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleInputChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
