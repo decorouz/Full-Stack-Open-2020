03bce33ef3c1a16a6de56a217204489322840045
Biola <abiola2326@gmail.com>
2020-03-25 01:33:10 +0100

    2.5: phonebook

--- part2/phonebook/src/components/AddContact.js
+++ part2/phonebook/src/components/AddContact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 const AddContact = ({
   persons,
@@ -24,7 +25,9 @@ const AddContact = ({
     if (isFound) {
       window.alert(`${newName.trim()} is already added to phonebook`);
     } else {
-      setPersons(persons.concat(newPerson));
+      axios.post('http://localhost:3001/persons', newPerson).then(response => {
+        setPersons(persons.concat(response.data));
+      });
       setNewName('');
       setNewNumber('');
     }
